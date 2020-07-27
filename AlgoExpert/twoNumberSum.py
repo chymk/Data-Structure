17d364e0c2c4eff40c2b7c2adf9aa38ee527fb55
Mukul <ch96mukul@gmail.com>
2020-07-28 00:17:14 +0530

    twoNumberSum 2 solutions

---
+++ AlgoExpert/twoNumberSum.py
@@ -0,0 +1,27 @@
+'''Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum. If any two numbers in the input array sum up to the
+target sum, the function should return them in an array, in sorted order. If no two numbers sum up to the target sum, the function should return an empty array.
+Assume that there will be at most one pair of numbers summing up to the target sum.
+
+`Sample input: [3, 5, -4, 8, 11, 1, -1, 6], 10
+Sample output: [-1, 11]'''
+
+def twoNoSumsol1(array,targetSum):
+    for i in range(len(array)-1):
+        firstNum = array[i]
+        for j in range(i+1,len(array)):
+            secondNum  = array[j]
+            if firstNum + secondNum == targetSum:
+                return [firstNum, secondNum]
+    return []
+
+def twoNoSumsol2(array, targetSum):
+    for firstNum in array:
+        if targetSum-firstNum in array:
+            return [firstNum, targetSum-firstNum]
+    return []
+
+arr = [1,2,3,4,5,6,7,9]
+tar = 5
+
+print(twoNoSumsol1(arr,tar))
+print(twoNoSumsol2(arr,tar))
