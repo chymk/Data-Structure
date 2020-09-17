f8267d1cc1abca3d8b1949010e98d74a30d63ca5
Mukul <ch96mukul@gmail.com>
2020-09-17 10:04:23 +0530

    remove duplicates from Linked List

--- LinkedList/SinglyLinkedList/Remove_Duplicates_From_LinkedList.py
+++ LinkedList/SinglyLinkedList/Remove_Duplicates_From_LinkedList.py
@@ -2,11 +2,13 @@ class Node:
     def __init__(self,data=None):
         self.data = data
         self.next = None
+        self.flag = False
 
 class LinkedList:
     def __init__(self):
         self.head = None
         self.count = 0
+
 
     def printLinkedList(self):
         current = self.head
@@ -26,42 +28,39 @@ class LinkedList:
             current.next = newNode
             newNode.next = None
 
-
-
-    def is_Palindrome(self):
-        stack = []
+    def removeDuplicates(self):
+        s = set()
         current = self.head
-        while current:
-            stack.append(current.data)
+        prev = None
+        while current is not None:
+            if current.data in set:
+                if current == self.head:
+                    self.head = current.next
+                elif current.next is None:
+                    prev.next = None
+                else:
+                    prev.next = current.next
+            s.add(current.data)
+            prev = current
             current = current.next
-        current = self.head
-        isPalind = False
-        while current:
-            i = stack.pop()
+        return
 
-            if i == current.data :
-                print(i,current.data)
-                isPalind = True
-            else:
-                isPalind = False
-                break
-            current = current.next
-        return isPalind
 
 
 lList = LinkedList()
-lList.head = Node(1)
-#a = Node(2)
-#b = Node(1)
-'''lList.head.next = a
+lList.head = Node(5)
+a = Node(6)
+b = Node(7)
+lList.head.next = a
 a.next = b
-lList.insertAtEnd(4)
-lList.insertAtEnd(3)
+lList.insertAtEnd(9)
 lList.insertAtEnd(2)
-#lList.insertAtEnd(1)'''
-
-
+lList.insertAtEnd(1)
+lList.insertAtEnd(0)
+lList.insertAtEnd(0)
+lList.insertAtEnd(3)
+lList.insertAtEnd(4)
 
 lList.printLinkedList()
-
-print("is Palindrome : ",lList.is_Palindrome())
+lList.removeDuplicates()
+lList.printLinkedList()
