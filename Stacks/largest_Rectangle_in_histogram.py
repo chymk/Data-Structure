6943d85171b9ca10b2ffaef8a899a540f107cd91
Mukul <ch96mukul@gmail.com>
2020-10-07 16:45:47 +0530

    updated Solution

--- Stacks/largest_Rectangle_in_histogram.py
+++ Stacks/largest_Rectangle_in_histogram.py
@@ -8,18 +8,27 @@ def largestRectangleArea(R):
                 minheight = min(minheight,R[k])
 
             maxArea = max(maxArea,minheight*(j-i+1))
+    return maxArea
 
-def largestRectangleArea(R):
+def largestRectangleArea1(R):
     maxArea =0
     for i in range(len(R)):
         minheight = R[i]
-        for j in range(i,)
+        for j in range(i,len(R)):
+            minheight = min(minheight,R[j])
+            maxArea = max(maxArea,(minheight*((j-i)+1)))
+    return maxArea
 
+def largestRectangleArea2(R):
+    maxArea =0
 
     return maxArea
+
 #Time - O(n^3)
 print(largestRectangleArea([2,1,5,6,2,3]))
 
 #Time - O(n^2)
-
 print(largestRectangleArea1([2,1,5,6,2,3]))
+
+# Time - O(n)
+print(largestRectangleArea2([2,1,5,6,2,3]))
