df5196ace922831385ae836ae72a4fc27ec98efb
Mukul <ch96mukul@gmail.com>
2020-10-09 23:49:49 +0530

    updated Queue Solution

---
+++ Queue/Creating_Queue_Using_Array.py
@@ -0,0 +1,19 @@
+class Queue:
+    def __init__(self,capacity):
+        self.front = self.size = 0
+        self.rear = capacity - 1
+        self.Q = [None]*capacity
+        self.capacity = capacity
+
+    def isFull(self):
+        self.size == self.capacity
+
+    def isEmpty(self):
+        self.size == 0
+
+    def Enqueue(self,item):
+        if self.isFull():
+            print("Queue is full")
+            return
+        self.rear = (self.rear+1) % self.capacity
+
