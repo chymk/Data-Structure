0d7d284f3418bd9868f887d9ec71a1fdd8f658ef
Mukul <ch96mukul@gmail.com>
2020-10-19 22:41:39 +0530

    updated  Solution

--- Trees/Classify_Traversal.py
+++ Trees/Classify_Traversal.py
@@ -1,4 +1,4 @@
-# Preorder
+# Preorder - DLR
 
 def recursivePreorder(root,result):
     if not root:
@@ -22,7 +22,7 @@ def preorderIterative(root,result):
             stack.append(node.left)
 
 
-# Inorder
+# Inorder - LDR
 
 def recursiveInorder(root,result):
     if not root:
@@ -33,8 +33,48 @@ def recursiveInorder(root,result):
 
 
 def InorderIterative(root,result):
-    if not root:
+    if not root: 
         return
     stack = []
     node = root
-    while stack:
+    while stack and node:
+        if node:
+            node = node.left
+            stack.append(node)
+        else:
+            node = stack.pop()
+            result.append(node.data)
+            node = node.right
+
+#Postorder -  LRD
+
+def recursivePostorder(root, result):
+    if not root:
+        return
+    recursivePostorder(root.left, result)
+    recursivePostorder(root.right,result)
+    result.append(root.data)
+
+
+def PostorderIterative(root,result):
+    if not root:
+        return
+    visited = set()
+    stack = []
+    node = root
+    while stack and node:
+        if node:
+            stack.append(node)
+            node = node.left
+
+        else:
+            node = stack.pop()
+            if node.right and node.right not in visited:
+                stack.append(node)
+                node = node.right
+            else:
+                visited.add(node)
+                result.append(node.data)
+                node = None
+
+
