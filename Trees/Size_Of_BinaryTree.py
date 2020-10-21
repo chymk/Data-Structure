abf096badf5b22ab80a5be5d4d6059b4f5f58329
Mukul <ch96mukul@gmail.com>
2020-10-22 00:09:09 +0530

    updated  Solution

--- Trees/Size_Of_BinaryTree.py
+++ Trees/Size_Of_BinaryTree.py
@@ -2,3 +2,11 @@ def SizeOfBinaryTree(root):
     if root is None:
         return 0
     return SizeOfBinaryTree(root.left)+SizeOfBinaryTree(root.right)+1
+
+def findSizeUSingLevelOrder(root):
+    if root is None:
+        return 0
+    q = Queue()
+    q.enque(root)
+    node = root
+    while not q.isEmpty():
