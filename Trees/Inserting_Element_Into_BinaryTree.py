62cee395fa7730104aab14aec1efa3020586c4ec
Mukul <ch96mukul@gmail.com>
2020-10-21 22:16:51 +0530

    updated  Solution

--- Trees/Inserting_Element_Into_BinaryTree.py
+++ Trees/Inserting_Element_Into_BinaryTree.py
@@ -16,4 +16,15 @@ class BinaryTree:
     def getRight(self):
         return self.right
 
-    def setLeft(self,newNode):
+    def insertLeft(self,newNode):
+        if self.left is None:
+            self.left = BinaryTree(newNode)
+        else:
+            temp = BinaryTree(newNode)
+            temp.left = self.left
+            self.left = temp
+
+    def insertRight(self,newNode):
+        if self.right is None:
+            self.right = BinaryTree(newNode)
+        else:
