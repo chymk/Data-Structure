6891d26ad40bc673672719fea13069d75eb777a2
Mukul <ch96mukul@gmail.com>
2020-10-22 09:23:07 +0530

    updated Tree Solution Solution

--- Trees/Size_Of_BinaryTree.py
+++ Trees/Size_Of_BinaryTree.py
@@ -1,3 +1,9 @@
+class Node:
+    def __init__(self,data=None):
+        self.data = data
+        self.left = None
+        self.right =None
+
 def SizeOfBinaryTree(root):
     if root is None:
         return 0
@@ -7,14 +13,27 @@ def findSizeUSingLevelOrder(root):
     if root is None:
         return 0
     size = 0
-    q = Queue()
-    q.enque(root)
+    q = []
+    q.append(root)
     node = root
-    while not q.isEmpty():
-        node =q.deque()
+    while q!=[]:
+        node =q.pop(0)
         size += 1
         if node.left is not None:
-            q.enque(node.left)
+            q.append(node.left)
         if node.right is not None:
-            q.enque(node.right)
+            q.append(node.right)
     return size
+
+if __name__== '__main__':
+    root = Node(2)
+    root.left = Node(7)
+    root.right = Node(5)
+    root.left.right = Node(6)
+    root.left.right.left = Node(1)
+    root.left.right.right = Node(11)
+    root.right.right = Node(9)
+    root.right.right.left = Node(4)
+
+    print(SizeOfBinaryTree(root))
+    print(findSizeUSingLevelOrder(root))
