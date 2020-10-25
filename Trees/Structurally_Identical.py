654fa517d20c49c30c6545472b3ff48493c37e2d
Mukul <ch96mukul@gmail.com>
2020-10-25 14:31:42 +0530

    updated Structurally Identical Solution

--- Trees/Structurally_Identical.py
+++ Trees/Structurally_Identical.py
@@ -4,7 +4,7 @@ def StructuralComparison(root1,root2):
         return True
     if node1.data!=node2.data or (node1.left and not node2.left) or (node1.right and not node2.right) or (not node1.left and node2.left)or \
         (not node1.right and  node2.right):
-        return False:
+        return False
     left = StructuralComparison(node1.left,node2.left) if node1.left and node2.left else True
     right = StructuralComparison(node1.right,node2.right) if node1.right and node2.right else True
     return left,right
