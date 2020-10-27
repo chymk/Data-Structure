7dd3aad7cbf821c93ab3983956ef4d10aaec87aa
Mukul <ch96mukul@gmail.com>
2020-10-27 22:57:49 +0530

    updated BinaryTree Solution

--- Trees/Level_Order_Traversal.py
+++ Trees/Level_Order_Traversal.py
@@ -2,6 +2,13 @@
 While traversing L, keep all the elements l+1 in queue
 Go the next level and visit all the nodes
 Repeat until all the levels are completed'''
+
+class Node:
+    def __init__(self,data=None):
+        self.data = data
+        self.left = None
+        self.right = None
+
 def levelOrderTraversal(root,result):
     if root is None:
         return
@@ -11,8 +18,21 @@ def levelOrderTraversal(root,result):
 
     while q != []:
         node = q.pop(0)
-        result.append(node.getData())
-        if node.getLeft() is not None:
-            q.append(node.getLeft())
-        if node.getRight is not None:
-            q.append(node.getRight())
+        result.append(node.data)
+        if node.left is not None:
+            q.append(node.left)
+        if node.right is not None:
+            q.append(node.right)
+    print(result)
+
+if __name__== '__main__':
+    root = Node(2)
+    root.left = Node(7)
+    root.right = Node(5)
+    root.left.right = Node(6)
+    root.left.right.left = Node(1)
+    root.left.right.right = Node(3)
+    root.right.right = Node(9)
+    root.right.right.left = Node(4)
+
+    levelOrderTraversal(root,[])
