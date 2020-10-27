00e3e645152be929b54b87837a48fd4622edce4e
Mukul <ch96mukul@gmail.com>
2020-10-27 22:53:33 +0530

    updated BinaryTree Solution

--- Trees/Level_Order_Traversal.py
+++ Trees/Level_Order_Traversal.py
@@ -5,14 +5,14 @@ Repeat until all the levels are completed'''
 def levelOrderTraversal(root,result):
     if root is None:
         return
-    q = Queue.Queue()
-    q.put(root)
+    q = []
+    q.append(root)
     node = None
 
-    while not q.isempty():
-        node = q.get()
+    while q != []:
+        node = q.pop(0)
         result.append(node.getData())
         if node.getLeft() is not None:
-            q.put(node.getLeft())
+            q.append(node.getLeft())
         if node.getRight is not None:
-            q.put(node.getRight())
+            q.append(node.getRight())
