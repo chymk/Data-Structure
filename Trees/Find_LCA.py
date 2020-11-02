240d3f24408252de437f6b18f4a6813e1b32ae12
Mukul <ch96mukul@gmail.com>
2020-11-02 23:21:16 +0530

    Binary search tree

--- Trees/Find_LCA.py
+++ Trees/Find_LCA.py
@@ -24,6 +24,7 @@ if __name__== '__main__':
     root = Node(2)
     root.left = Node(7)
     root.right = Node(5)
+
     root.left.right = Node(6)
     root.left.right.left = Node(1)
     root.left.right.right = Node(11)
