cba9a3b516da0a2f4718d6292a449a3348402775
Mukul <ch96mukul@gmail.com>
2020-11-04 19:28:28 +0530

    Binary search tree

---
+++ Trees/BST_Delete_Element.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data=None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def insert(root,key):
+    if root is None:
+        return Node(key)
+    if root.data == key:
+        return root
+    if key < root.data:
+        root.left = insert(root.left,key)
+    else:
+        root.right = insert(root.right,key)
+    return root
+
+def delete(root,key):
+    if root is None:
+        print("Tree is empty")
+
+
+r = Node(50)
+r = insert(r, 30)
+r = insert(r, 20)
+r = insert(r, 40)
+r = insert(r, 70)
+r = insert(r, 60)
+r = insert(r, 80)
+
+print("Predecessor : ",predecessor(r))
+print("Successor : ",successor(r))
