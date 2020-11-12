eff8b98f6ae9d22952c5452a32563650ed9b54a1
Mukul <ch96mukul@gmail.com>
2020-11-12 19:04:19 +0530

    Construct Heap

---
+++ Priority Queues & Heaps/Binary_Heaps.py
@@ -0,0 +1,24 @@
+class Heap:
+    def __init__(self):
+        self.heapList = [0]
+        self.size = 0
+
+    def parent(self,index):
+        # node at ith Location it's parent will be at i-1/2 location
+        return index/2
+
+    def leftChild(self,index):
+        return 2*index+1
+
+    def rightChild(self,index):
+        return 2*index+1
+
+    def getMaximum(self):
+        if self.size == 0:
+            return -1
+        return self.heapList[0]
+
+    def getMinimum(self):
+        if self.size == 0:
+            return -1
+        return self.heapList[0]
