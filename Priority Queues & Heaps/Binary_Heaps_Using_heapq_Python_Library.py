7560a7be98fb7d6031e00a0ed60686e9b51624d8
Mukul <ch96mukul@gmail.com>
2020-11-13 15:29:38 +0530

    Construct Min Heap using Python lirary functions

---
+++ Priority Queues & Heaps/Binary_Heaps_Using_heapq_Python_Library.py
@@ -0,0 +1,47 @@
+# import the heap functions from Python library
+from heapq import heappop, heappush, heapify
+
+
+# heappop - pop and return the samallest element from heap
+# heappush - push the value onto the heap, maintaining the heap invarient
+# heapify - transform list into heap, inplace, inlinear time
+
+class Minheap:
+
+    def __init__(self):
+        self.heap = []
+
+    def parent(self,i):
+        return (i-1)/2
+
+    def insertKey(self,k):
+        heappush(self.heap,k)
+
+    def decreaseKey(self,i,newval):
+        self.heap[i] = newval
+        while i!=0 and self.heap[self.parent(i)] > self.heap[i]:
+            # Swap heap[i] with heap[parent(i)]
+            self.heap[i],self.heap[self.parent(i)] = self.heap[self.parent(i)],self.heap[i]
+
+    def extractMin(self):
+        return heappop(self.heappop())
+
+    def deleteKey(self,i):
+        self.decreaseKey(i,float('-inf'))
+        self.extractMin()
+
+    def getMin(self):
+        return self.heap[0]
+
+
+heapObj = Minheap()
+heapObj.insertKey(3)
+heapObj.insertKey(2)
+heapObj.insertKey(1)
+heapObj.insertKey(15)
+heapObj.insertKey(5)
+heapObj.insertKey(4)
+heapObj.insertKey(45)
+
+
+print(heapObj.extractMin())
