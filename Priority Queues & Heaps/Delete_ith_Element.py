0fd652bdfcbd81297143416c628f544e4f9475f2
Mukul <ch96mukul@gmail.com>
2020-11-15 19:39:50 +0530

    Max Element in a min heap

---
+++ Priority Queues & Heaps/Delete_ith_Element.py
@@ -0,0 +1,10 @@
+def delete(self,i):
+    if i<self.size:
+        print("Location Outbound")
+        return
+    key = self.array[i]
+    self.array[i] = self.array[self.size-1]
+    self.size = self.size-1
+    self.percolateDown(i)
+    return key
+
