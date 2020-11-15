9ae020931e344ace9f8f4740d8c72b08b85719fa
Mukul <ch96mukul@gmail.com>
2020-11-15 19:41:23 +0530

    Delete ith element from heap

--- Priority Queues & Heaps/Delete_ith_Element.py
+++ Priority Queues & Heaps/Delete_ith_Element.py
@@ -2,6 +2,7 @@ def delete(self,i):
     if i<self.size:
         print("Location Outbound")
         return
+
     key = self.array[i]
     self.array[i] = self.array[self.size-1]
     self.size = self.size-1
