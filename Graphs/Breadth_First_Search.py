d9ba7ccc1ef4767a9145178383e596dcbc50b907
Mukul <ch96mukul@gmail.com>
2020-11-25 00:00:32 +0530

    Breadth First Search

--- Graphs/Breadth_First_Search.py
+++ Graphs/Breadth_First_Search.py
@@ -7,7 +7,18 @@ class Graph:
     def add(self,u,v):
         self.graph[u].append(v)
 
-
+    def BFS(self,s):
+        queue = []
+        visited = [False]*(len(self.graph))
+        queue.append(s)
+        visited[s] = True
+        while queue:
+            s= queue.pop(0)
+            print(s,end=" ")
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
 
 
 
