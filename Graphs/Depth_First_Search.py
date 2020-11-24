76d5973d7078bd9152e9e5faaf342e7c48dfb969
Mukul <ch96mukul@gmail.com>
2020-11-24 23:46:37 +0530

    Graph Adj List

---
+++ Graphs/Depth_First_Search.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add(self,u,v):
+        self.graph[u].append(v)
+
+    def BFS(self,s):
+        queue = []
+        visited = [None]*(len(self.graph))
+        queue.append(s)
+        visited[s] = True
+        while queue:
+            if
+
+g = Graph()
+g.add(0,1)
+g.add(0,2)
+g.add(1,2)
+g.add(2,0)
+g.add(2,3)
+g.add(3,3)
+
+print ("Following is Breadth First Traversal starting from vertex 2",g.BFS(2))
