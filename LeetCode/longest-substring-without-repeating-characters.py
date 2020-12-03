394ad9463c3de5caee4207f0ddca5baf4632a5cf
Mukul <ch96mukul@gmail.com>
2020-12-03 21:48:07 +0530

    Leetcode solutions

---
+++ LeetCode/longest-substring-without-repeating-characters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLongestSubstring(self, str):
+        return 1
+
+s = Solution()
+print("abcabcbb  Length of substring - ",s.lengthOfLongestSubstring("abcabcbb"))
+
+print("bbbbbb  Length of substring - ",s.lengthOfLongestSubstring("bbbbbb"))
+
+print("pwwkew  Length of substring - ",s.lengthOfLongestSubstring("pwwkew"))
