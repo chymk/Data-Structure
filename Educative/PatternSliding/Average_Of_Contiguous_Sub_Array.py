d1f69ef6175c230da6ef5c518ddf38eb03382576
Mukul <ch96mukul@gmail.com>
2021-01-03 17:30:22 +0530

    Average of K contiguos sub array

--- Educative/PatternSliding/Average_Of_Contiguous_Sub_Array.py
+++ Educative/PatternSliding/Average_Of_Contiguous_Sub_Array.py
@@ -1,8 +1,19 @@
-def averageOfContiguosSubArray(arr,k):
+def averageOfContiguousSubArray(arr,k):
     n = len(arr) - k + 1
     avg = []
     for i in range(n):
         avg.append(sum(arr[i:k+i])/k)
     print(avg)
 
-averageOfContiguosSubArray([1, 3, 2, 6, -1, 4, 1, 8, 2],5)
+def averageOfContiguousSubArray2(arr,k):
+    result = []
+    windowStart, windowSum = 0,0.0
+    for windowEnd in range(len(arr)):
+        windowSum += arr[windowEnd]
+        if windowEnd >= k-1:
+            result.append(windowSum/k)
+            windowSum -= arr[windowStart]
+            windowStart += 1
+    print(result)
+
+averageOfContiguousSubArray2([1, 3, 2, 6, -1, 4, 1, 8, 2],5)
