c6485fe65a081747c7845c1f8359700d41202752
Mukul <ch96mukul@gmail.com>
2021-01-03 21:53:17 +0530

    smallest sub Array

--- Educative/PatternSliding/Smallest_Sub_Array_With_Given_Sum.py
+++ Educative/PatternSliding/Smallest_Sub_Array_With_Given_Sum.py
@@ -1,15 +1,14 @@
 def smallestSubArray(arr,sum):
-    maximum = 0
-    for i in range(len(arr)):
-        if sum in arr:
-            return i+1
-        else:
-            maximum = max(arr)
-            if maximum > sum:
-                return i+1
-            else:
-                arr.remove(maximum)
-                sum -= maximum
+    windowStart, windowSum, minLength = 0,0,float('inf')
+    for windowEnd in range(len(arr)):
+        windowSum += arr[windowEnd]
+        while windowSum >= sum:
+            minLength = min(minLength,windowEnd-windowStart+1)
+            windowSum -= arr[windowStart]
+            windowStart += 1
+    if minLength == float('inf'):
+            return 0
+    return minLength
 
 print(smallestSubArray([2, 1, 5, 2, 3, 2],7))
 print(smallestSubArray([2, 1, 5, 2, 8],7))
