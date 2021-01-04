dbcff7d1bc8ccc3607b8945d91c9a5bcf1c9abfd
Mukul <ch96mukul@gmail.com>
2021-01-04 11:33:53 +0530

    unique longest substring length

--- Educative/PatternSliding/Unique_Longest_Substring_Length.py
+++ Educative/PatternSliding/Unique_Longest_Substring_Length.py
@@ -2,4 +2,22 @@
 Output: 2
 Explanation: The longest substring without any repeating characters is "ab".'''
 def uniqueSubString(str):
-    return
+    windowStart, count = 0,0
+    q = []
+    for windowEnd in range(len(str)):
+        if str[windowEnd] not in q:
+            q.append(str[windowEnd])
+            count = max(count,len(q))
+            print(q)
+        else:
+            q.append(str[windowEnd])
+            print(q)
+            while len(q) != len(set(q)):
+                q.pop(0)
+                print("Inside While",q)
+
+    print(count)
+
+uniqueSubString("abbbb")
+uniqueSubString("aabccbb")
+uniqueSubString("abccde")
