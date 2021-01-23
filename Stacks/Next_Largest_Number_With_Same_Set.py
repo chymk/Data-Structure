d981410d886750ddb77c797629b514504ce0e16b
Mukul <ch96mukul@gmail.com>
2021-01-23 20:41:05 +0530

    Next Largest Number with same set

---
+++ Stacks/Next_Largest_Number_With_Same_Set.py
@@ -0,0 +1,25 @@
+def nextLargest(num):
+    stk = []
+    num = str(num)
+    str1 = ''
+    for i in range(len(num)):
+        stk.append(num[i])
+    print(stk)
+
+    while stk != []:
+        s = stk.pop()
+        print(s,' ',str1)
+        if len(str1) and int(s) < int(str1[-1]):
+            str1 = str1[-1]+"".join(sorted(str1[:-1]+s))
+            print("inside if : ",str1)
+            while stk != []:
+                s = stk.pop()
+                str1 = s+str1
+            return str1
+        else:
+            str1 = s + str1
+    return -1
+
+num = 1234986
+print(nextLargest(num))
+print(nextLargest(218765))
