7f89da0fd3b34c0ec0571953af9292c1cd08c36e
Mukul <ch96mukul@gmail.com>
2021-05-01 15:36:29 +0530

    selectionSortSort Code Updated

---
+++ Sorting Algo/selectionSort.py
@@ -0,0 +1,13 @@
+def selectionSort(arr):
+    for i in range(len(arr)):
+        minIdx = i
+        for j in range(i+1,len(arr)):
+            if arr[minIdx]>arr[j]:
+                minIdx = j
+        arr[i],arr[minIdx] = arr[minIdx],arr[i]
+
+
+
+arr = [1,7,3,9,5,2,6,4]
+selectionSort(arr)
+print(arr)
