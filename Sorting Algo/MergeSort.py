543709483053ad8c7f5490a111ef64456428c284
Mukul <ch96mukul@gmail.com>
2021-05-01 11:47:54 +0530

    MerSort Code Updated

--- Sorting Algo/MergeSort.py
+++ Sorting Algo/MergeSort.py
@@ -1,4 +1,4 @@
-def merSort(arr,l,r):
+def mergeSort(arr,l,r):
   if l<r:
     m = l+(r-l)//2
     mergeSort(arr,l,m)
@@ -10,29 +10,31 @@ def merge(arr,l,m,r):
   n2 = r-m
   L = [0]*n1
   R = [0]*n2
+
   for i in range(n1):
     L[i] = arr[l+i]
   for j in range(n2):
-    L[j] = arr[m+j]
-  i = 0
-  j = 0
-  k = 0
+    R[j] = arr[m+1+j]
+
+  i,j,k = 0,0,l
   while i<n1 and j<n2:
-    if L[i]<R[j]:
+    if L[i]<=R[j]:
       arr[k] = L[i]
       i+=1
     else:
       arr[k] = R[j]
       j+=1
     k+=1
-  
+
   while i<n1:
     arr[k] = L[i]
     i+=1
     k+=1
   while j<n2:
-    arr[k] = L[j]
+    arr[k] = R[j]
     j+=1
     k+=1
-  
-  
+
+arr = [1,7,3,9,5,2,6,4]
+mergeSort(arr,0,len(arr)-1)
+print(arr)
