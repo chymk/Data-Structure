7d83fd2ec038d7066e686c1b29c8e240d14439ef
Mukul <ch96mukul@gmail.com>
2021-05-02 18:59:58 +0530

    insertionSort Code Updated

---
+++ Sorting Algo/insertionSort.py
@@ -0,0 +1,11 @@
+def insertionSort(A):
+    for i in range(1,len(A)):
+        key = A[i]
+        j = i-1
+        while j>=0 and A[j]>key:
+            A[j+1],A[j] = A[j],A[j+1]
+            j-=1
+
+arr = [1,7,3,9,5,2,6,4]
+insertionSort(arr)
+print(arr)
