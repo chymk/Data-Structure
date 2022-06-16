57d4933f1a58ff9dcbe7149772beae04ad21acff
Mukul Choudhary <ch96mukul@gmail.com>
2022-06-16 22:31:07 +0530

    Update Bubble_Sort.py

--- AlgoExpert/Bubble_Sort.py
+++ AlgoExpert/Bubble_Sort.py
@@ -4,7 +4,6 @@ Use the Bubble Sort algorithm to sort the array.
 Sample input: [8, 5, 2, 9, 5, 6, 3]
 Sample output: [2, 3, 5, 5, 6, 8, 9]'''
 
-
 def bubble_sort(array):
     counter = 0
     is_sorted = False
@@ -24,7 +23,5 @@ def swap_array_places(array,i,j):
     array[i],array[j] = array[j],array[i]
     return array
 
-
 arr=[8, -17,5, 2, 9, 5, 6, 3,-1]
-
 print(bubble_sort(arr))
