55ac5c4345393e05d340f8700fcf3347a1bb3895
Mukul <ch96mukul@gmail.com>
2022-07-25 23:57:28 +0530

    Undo redo operation

---
+++ SC/Stacks/Undo_Redo.py
@@ -0,0 +1,34 @@
+class Operation:
+    reverse_operator_map = {'*':'/','/':'*','+':'-','-':'+'}
+    def __init__(self,num):
+        self.num = num
+        self.undo = []
+        self.redo = []
+    def printNum(self):
+        print(self.num)
+    def calculate(self,operand,operator,undo=False):
+        match operator:
+            case '+':
+                self.num = self.num + operand
+            case '-':
+                self.num = self.num - operand
+            case '*':
+                self.num = self.num * operand
+            case '/':
+                self.num = self.num / operand
+        if not undo:
+            self.undo.append([operand,operator])
+            self.redo = []
+        return self.num
+
+    def undo_operation(self):
+        temp = self.undo.pop()
+        self.calculate(temp[0],self.reverse_operator_map[temp[1]],True)
+        self.redo.append(temp)
+
+    def redo_operaation(self):
+        temp = self.redo.pop()
+        self.calculate(temp[0],temp[1],True)
+
+
+
